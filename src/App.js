1226196841aea30342ec632a2c57fdcba3dbcd05
Pulkit Kumar <pulkitnishad789@gmail.com>
2024-05-12 15:46:49 +0530

    made a To-Do list app with React.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import { Checkbox } from '@mui/material';
+
 
 function App() {
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+ const addData = () => {
+    
+    if(task === ''){
+      alert('Please enter task')
+    } else {
+      setTaskList([...taskList, task]);
+      setTask('');
+    }}
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Header/>
+      </div>
+      <div className='form'>
+         <TextField value={task} 
+         onChange={(e) => setTask(e.target.value)}
+         id="outlined-basic" label="Enter Task" variant="outlined" />
+        <div className='add'>
+         <Button onClick={addData} variant="contained" >Add</Button>
+         </div>
+      </div>
+      
+
+      </>
   );
 }
 
