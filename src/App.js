a9227ae0ac02482116fab3985a3e28b188b48141
Pulkit Kumar <pulkitnishad789@gmail.com>
2024-05-15 00:26:30 +0530

    Now its fully working...

--- src/App.js
+++ src/App.js
@@ -1,38 +1,76 @@
-import './App.css';
-import Header from './components/header';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { useState } from 'react';
-import { Checkbox } from '@mui/material';
-
+import "./App.css";
+import Header from "./components/header";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+import { Checkbox } from "@mui/material";
 
 function App() {
-  const [task, setTask] = useState('');
+  const [task, setTask] = useState("");
   const [taskList, setTaskList] = useState([]);
- const addData = () => {
-    
-    if(task === ''){
-      alert('Please enter task')
+
+  const addData = (e) => {
+    e.preventDefault();
+    if (task === "") {
+      alert("Please enter task");
     } else {
       setTaskList([...taskList, task]);
-      setTask('');
-    }}
+      setTask("");
+    }
+  };
+
+  const handleDelete = (index) => {
+    const newList = [...taskList];
+    newList.splice(index, 1);
+    setTaskList(newList);
+  };
+
   return (
     <>
-    <div className="App">
-      <Header/>
+      <div className="App">
+        <Header />
       </div>
-      <div className='form'>
-         <TextField value={task} 
-         onChange={(e) => setTask(e.target.value)}
-         id="outlined-basic" label="Enter Task" variant="outlined" />
-        <div className='add'>
-         <Button onClick={addData} variant="contained" >Add</Button>
-         </div>
+      <form className="form" onSubmit={addData}>
+        <TextField
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          id="outlined-basic"
+          label="Enter Task"
+          variant="outlined"
+        />
+        <div className="add">
+          <Button type="submit" variant="contained">
+            Add
+          </Button>
+        </div>
+      </form>
+      <div className="task-list-head">
+        <h1>Task List</h1>
+        <h1>Done</h1>
+        <h1>Delete</h1>
       </div>
-      
-
-      </>
+      <table className="task-table">
+        <tbody>
+          {taskList.map((task, index) => (
+            <tr key={index}>
+              <td>{task}</td>
+              <td>
+                <Checkbox />
+              </td>
+              <td>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => handleDelete(index)}
+                >
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 const Header =() => {
   return (
     <div className="header"> 
-    <h1>To-Do-List</h1>
+    <h1 className="heading">To-Do-List</h1>
     </div>
   )
 }
